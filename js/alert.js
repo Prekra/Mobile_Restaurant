4da0d21750c90e6fcf5309a138023032b68bfd2e
Prekra <77902653+Prekra@users.noreply.github.com>
2021-06-08 23:34:54 -0500

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function alerta(){
+    swal({
+        icon:"success",
+        text:"Bienvenid@ a disfrutar lo mejor de la gastronomía en un lugar totalmente fuera de lo común, reservas, eventos temáticos, karaoke, todo lo que buscas esta aquí!"
+    })
+}
+
+
+mensaje = setTimeout(alerta,1000)
